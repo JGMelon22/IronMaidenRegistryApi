274e2b77287875d30170251f4c66070c4d29d394
JGMelon22 <jgvasconcellos22@gmail.com>
2023-12-26 14:15:14 -0300

    fix: removing unecessary property frim MemberResult class

--- DTOs/Member/MemberResult.cs
+++ DTOs/Member/MemberResult.cs
@@ -3,8 +3,6 @@ namespace IronMaidenRegistry.DTOs.Member;
 public record MemberResult
 {
     public Guid Id { get; init; }
-    public int MyProperty { get; init; }
     public string FullName { get; init; } = null!;
     public DateOnly BirthDate { get; init; }
-
 }

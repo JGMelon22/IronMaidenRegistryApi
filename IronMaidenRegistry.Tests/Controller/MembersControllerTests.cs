b43308885290b9690999dc87ceb3467cefacfa28
JGMelon22 <jgvasconcellos22@gmail.com>
2023-12-31 12:52:33 -0300

    feat: developing MembersController Tests (GetMemberByIdAsync, AddMemberAsync, UpdateMemberAsync and RemoveMemberAsync)

--- IronMaidenRegistry.Tests/Controller/MembersControllerTests.cs
+++ IronMaidenRegistry.Tests/Controller/MembersControllerTests.cs
@@ -35,4 +35,69 @@ public class MembersControllerTests
         result.Should().NotBeNull();
         result.Should().BeOfType<Task<IActionResult>>();
     }
+
+    [Fact]
+    public void MembersController_GetMemberByIdAsync_ReturnsMembers()
+    {
+        // Arrange
+        Guid id = Guid.NewGuid();
+        var member = A.Fake<ServiceResponse<MemberResult>>();
+        A.CallTo(() => _memberRepository.GetMemberByIdAsync(id)).Returns(member);
+
+        // Act
+        var result = _membersController.GetMemberByIdAsync(id);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<Task<IActionResult>>();
+    }
+
+    [Fact]
+    public void MembersController_AddMemberAsync_ReturnsMembers()
+    {
+        // Arrange 
+        var newMember = A.Fake<MemberInput>();
+        var memberResult = A.Fake<ServiceResponse<MemberResult>>();
+        A.CallTo(() => _memberRepository.AddMemberAsync(newMember)).Returns(memberResult);
+
+        // Act
+        var result = _membersController.AddMemberAsync(newMember);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<Task<IActionResult>>();
+    }
+
+    [Fact]
+    public void MembersController_UpdateMemberAsync_ReturnsMembers()
+    {
+        // Arrange 
+        Guid id = Guid.NewGuid();
+        var updatedMember = A.Fake<MemberInput>();
+        var memberResult = A.Fake<ServiceResponse<MemberResult>>();
+        A.CallTo(() => _memberRepository.AddMemberAsync(updatedMember)).Returns(memberResult);
+
+        // Act
+        var result = _membersController.UpdateMemberAsync(id, updatedMember);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<Task<IActionResult>>();
+    }
+
+    [Fact]
+    public void MembersController_RemoveMemberAsync_ReturnsMembers()
+    {
+        // Arrange
+        Guid id = Guid.NewGuid();
+        var success = A.Fake<ServiceResponse<bool>>();
+        A.CallTo(() => _memberRepository.RemoveMemberAsync(id)).Returns(success);
+
+        // Act
+        var result = _membersController.RemoveMemberAsync(id);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<Task<IActionResult>>();
+    }
 }
